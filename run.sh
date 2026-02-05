1d2178ca3fe7e2aaef2925d9ed858e60c56939a2
CharlieC30 <charliechangintw@gmail.com>
2026-02-05 15:03:09 +0800

    Update run.sh and topatch.py for filopodia experiment paths and filenames

--- run.sh
+++ run.sh
@@ -9,7 +9,9 @@ NO_ALBUMENTATIONS_UPDATE=1 python train.py --yaml aisr --prj default/max10skip4 
 # filopodia on GHCL01
 cd /home/aero/charliechang/projects/IsoScopeXX/
 NO_ALBUMENTATIONS_UPDATE=1 python train.py --yaml filopodia_GHCL01 --prj default/max10skip4 --env GHCL01 --nocut --downbranch 2 --cropz 32
+NO_ALBUMENTATIONS_UPDATE=1 python train.py --yaml filopodia_GHCL01 --prj default/ --env GHCL01 --nocut --downbranch 2 --cropz 32
 
 # filopodia on NTHU_Gary3
 cd IsoScopeXX
 NO_ALBUMENTATIONS_UPDATE=1 python train.py --yaml filopodia_NTHU_Gary3 --prj default/max10skip4 --env NTHU_Gary3 --nocut --downbranch 2 --cropz 32
+NO_ALBUMENTATIONS_UPDATE=1 python train.py --yaml filopodia_NTHU_Gary3 --prj default/ --env NTHU_Gary3 --nocut --downbranch 2 --cropz 32
--- topatch.py
+++ topatch.py
@@ -112,9 +112,9 @@ def z_rescale(xx, trd=6):
 
 
 if __name__ == "__main__":
-    root = '/media/aero/HDD01/CharlieChang/Data/filopodia/'
+    root = '/media/aero/HDD01/CharlieChang/Data/filopodia_tif/'
     suffix = ''
-    npy0 = tiff.imread(root + '10X_G0431;6xGFP_incubator_10xSp_4d_Ch-GFP_G-ch-bio_SA635_1_Stitch_crop2048.tif')
+    npy0 = tiff.imread(root + '10X_G0431;6xGFP_incubator_10xSp_4d_Ch-GFP_G-ch-bio_SA635_9_R.tif')
     #npy1 = tiff.imread(root + 'dist0.tif')
 
     #tif_to_patches([npy0, npy1],
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     #               prefix='naivevmat1', ftr=-10, zrescale=None)
 
     tif_to_patches([npy0],
-                   destination=['filopodia_patches_crop2048/'],
+                   destination=['filopodia_patches_9_R/'],
                    dh=(64, 256, 256), step=(64, 256, 256), permute=None,
                    trd=(None, None), norm=('11', '11'),
                    prefix='', ftr=-10, zrescale=None, 
